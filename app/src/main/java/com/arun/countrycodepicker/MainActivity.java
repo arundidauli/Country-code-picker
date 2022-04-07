b26f2c29e48485d52bf8417f0096a90996788d63
arunandroid <arun.kumer@pvtltd.com>
2022-04-07 18:23:29 +0530

    first commit

--- app/src/main/java/com/arun/countrycodepicker/MainActivity.java
+++ app/src/main/java/com/arun/countrycodepicker/MainActivity.java
@@ -3,12 +3,21 @@ package com.arun.countrycodepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.arun.country_code_picker.PhoneUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "Country Code : "+ PhoneUtil.GetCountryZipCode(this));
+        Log.e(TAG, "Country Code : +"+ PhoneUtil.GetCountryZipCode(this));
+
     }
 }
